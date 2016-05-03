3f67d6d51a53c284b5d70fc874624d619a56a12e
s193956 <s193956@stud.hioa.no>
2016-05-03 12:25:07 +0200

    created model object Offer

---
+++ src/main/java/com/ryggsekk/promosoffers/model/Offer.java
@@ -0,0 +1,134 @@
+package com.ryggsekk.promosoffers.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="OFFERS")
+public class Offer {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="CATEGORY", nullable=false)
+    private String category;
+
+    @Column(name="NAME", nullable=false)
+    private String name;
+
+    @Column(name="PRICE", nullable=false)
+    private String price;
+
+    @Column(name="LOC_NAME", nullable=false)
+    private String locationName;
+
+    @Column(name="LOC_X", nullable=false)
+    private String locationX;
+
+    @Column(name="LOC_Y", nullable=false)
+    private String locationY;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name= name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price= price;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName= locationName;
+    }
+
+    public String getLocationX() {
+        return locationX;
+    }
+
+    public void setLocationX(String locationX) {
+        this.locationX= locationX;
+    }
+
+    public String getLocationY() {
+        return locationY;
+    }
+
+    public void setLocationY(String locationY) {
+        this.locationY= locationY;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Offer))
+            return false;
+        Offer other = (Offer) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer [id=" + id + ", name=" + name+ ", price=" + price
+                + ", location name=" + locationName+ ", location X=" + locationX
+                + ", location Y=" + locationY +"]";
+    }
+
+
+}

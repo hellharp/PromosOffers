262b32df0a1e6731c5144260736ed41243619048
oddmm <oddmm@DESKTOP-7KT1LAU>
2016-05-03 15:35:28 +0200

    Controller oppdatert med imports, lagt til mapping for /newUser. Laft til formhandling

--- src/main/java/com/ryggsekk/promosoffers/controller/PromosOffersController.java
+++ src/main/java/com/ryggsekk/promosoffers/controller/PromosOffersController.java
@@ -2,9 +2,8 @@ package com.ryggsekk.promosoffers.controller;
  
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
-import com.ryggsekk.promosoffers.model.Offer;
-import com.ryggsekk.promosoffers.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
@@ -18,9 +17,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
  
+import com.ryggsekk.promosoffers.model.Offer;
+import com.ryggsekk.promosoffers.service.OfferService;
+import com.ryggsekk.promosoffers.model.User;
+import com.ryggsekk.promosoffers.model.UserProfile;
+import com.ryggsekk.promosoffers.service.UserProfileService;
+import com.ryggsekk.promosoffers.service.UserService;
+
 @Controller
 public class PromosOffersController {
+    
+    @Autowired
+    UserProfileService userProfileService;
+     
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
     public String homePage(ModelMap model) {
@@ -33,11 +46,6 @@ public class PromosOffersController {
         model.addAttribute("user", getPrincipal());
         return "admin";
     }
-
-
-
-
-
 
     private OfferService offerService;
 
@@ -114,6 +122,43 @@ public class PromosOffersController {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login?logout";
+    }
+    
+    @RequestMapping(value = "/newUser", method = RequestMethod.GET)
+    public String newRegistration(ModelMap model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "newuser";
+    }
+    
+    /*
+     * This method will be called on form submission, handling POST request It
+     * also validates the user input
+     */
+    @RequestMapping(value = "/newUser", method = RequestMethod.POST)
+    public String saveRegistration(@Valid User user,
+            BindingResult result, ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            System.out.println("There are errors");
+            return "newuser";
+        }
+        userService.save(user);
+         
+        System.out.println("First Name : "+user.getFirstName());
+        System.out.println("Last Name : "+user.getLastName());
+        System.out.println("SSO ID : "+user.getSsoId());
+        System.out.println("Password : "+user.getPassword());
+        System.out.println("Email : "+user.getEmail());
+        System.out.println("Checking UsrProfiles....");
+        if(user.getUserProfiles()!=null){
+            for(UserProfile profile : user.getUserProfiles()){
+                System.out.println("Profile : "+ profile.getType());
+            }
+        }
+         
+        model.addAttribute("success", "User " + user.getFirstName() + " has been registered successfully");
+        return "registrationsuccess";
     }
  
     private String getPrincipal(){

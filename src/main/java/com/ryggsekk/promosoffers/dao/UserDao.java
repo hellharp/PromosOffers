f9650c6582437bf936e3eb8cb7e8956f2f464d0a
oddmm <oddmm@DESKTOP-7KT1LAU>
2016-05-03 15:20:55 +0200

    Brukerlagring

--- src/main/java/com/ryggsekk/promosoffers/dao/UserDao.java
+++ src/main/java/com/ryggsekk/promosoffers/dao/UserDao.java
@@ -3,6 +3,8 @@ package com.ryggsekk.promosoffers.dao;
 import com.ryggsekk.promosoffers.model.User;
  
 public interface UserDao {
+    
+    void save(User user);
  
     User findById(int id);
      

7958b19b6e1f3e403500b35fd87b1a2ce8638932
oddmm <oddmm@DESKTOP-7KT1LAU>
2016-05-04 14:32:51 +0200

    Pyntet på kode

--- src/main/java/com/ryggsekk/promosoffers/service/UserService.java
+++ src/main/java/com/ryggsekk/promosoffers/service/UserService.java
@@ -1,12 +1,19 @@
 package com.ryggsekk.promosoffers.service;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
  
 import com.ryggsekk.promosoffers.model.User;
+import java.util.List;
  
 public interface UserService {
  
     void save(User user);
+    
     User findById(int id);
      
     User findBySso(String sso);
+    
+   
      
 }
